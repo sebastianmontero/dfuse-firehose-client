9c05b81d75834e9cad9732c49561a2330b48f0e6
Sebastian Montero <sebastian.montero@gmail.com>
2024-01-16 15:13:53 -0600

    Removed Decoder class. Developed dfclient to use the new pinax firehose.

--- run.go
+++ run.go
@@ -3,19 +3,18 @@ package main
 import (
 	"fmt"
 
-	pbcodec "github.com/dfuse-io/dfuse-eosio/pb/dfuse/eosio/codec/v1"
+	pbantelope "github.com/pinax-network/firehose-antelope/types/pb/sf/antelope/type/v1"
 	"github.com/rs/zerolog"
 	"github.com/sebastianmontero/dfuse-firehose-client/dfclient"
 	"github.com/sebastianmontero/slog-go/slog"
-	"github.com/streamingfast/bstream"
-	pbbstream "github.com/streamingfast/pbgo/dfuse/bstream/v1"
+	pbfirehose "github.com/streamingfast/pbgo/sf/firehose/v2"
 )
 
 type blockStreamHandler struct {
 	cursor string
 }
 
-func (m *blockStreamHandler) OnBlock(block *pbcodec.Block, cursor string, forkStep pbbstream.ForkStep) {
+func (m *blockStreamHandler) OnBlock(block *pbantelope.Block, cursor string, forkStep pbfirehose.ForkStep) {
 	// fmt.Println("On Block: ", block, "Cursor: ", cursor, "Fork Step:", forkStep)
 	fmt.Println("Cursor: ", cursor, "Fork Step:", forkStep)
 }
@@ -24,8 +23,8 @@ func (m *blockStreamHandler) OnError(err error) {
 	fmt.Println("On Error: ", err)
 }
 
-func (m *blockStreamHandler) OnComplete(cursor string, lastBlockRef bstream.BlockRef) {
-	fmt.Println("On Complete cursor: ", cursor, "Last Block Ref: ", lastBlockRef)
+func (m *blockStreamHandler) OnComplete(cursor string) {
+	fmt.Println("On Complete cursor: ", cursor)
 }
 
 func (m *blockStreamHandler) Cursor(cursor string) string {
@@ -36,11 +35,11 @@ type deltaStreamHandler struct {
 	cursor string
 }
 
-func (handler *deltaStreamHandler) OnDelta(delta *dfclient.TableDelta, cursor string, forkStep pbbstream.ForkStep) {
+func (handler *deltaStreamHandler) OnDelta(delta *dfclient.TableDelta, cursor string, forkStep pbfirehose.ForkStep) {
 	fmt.Println("Cursor: ", cursor, "Fork Step:", forkStep, "\nOn Delta: ", delta)
 }
 
-func (handler *deltaStreamHandler) OnHeartBeat(block *pbcodec.Block, cursor string) {
+func (handler *deltaStreamHandler) OnHeartBeat(block *pbantelope.Block, cursor string) {
 	fmt.Println("On Heartbeat, block num: ", block.Number, " cursor: ", cursor)
 }
 
@@ -48,17 +47,15 @@ func (handler *deltaStreamHandler) OnError(err error) {
 	fmt.Println("On Error: ", err)
 }
 
-func (handler *deltaStreamHandler) OnComplete(lastBlockRef bstream.BlockRef) {
-	fmt.Println("On Complete Last Block Ref: ", lastBlockRef)
+func (handler *deltaStreamHandler) OnComplete() {
+	fmt.Println("On Complete")
 }
 
 func main() {
-	dfuseEndpoint := "telos.firehose.eosnation.io:9000"
-	dfuseAPIKey := "dc6087c88050f3caeed46f22767c357c"
-	dfuseAuthURL := "https://auth.eosnation.io"
-	chainEndpoint := "https://mainnet.telos.net"
+	dfuseEndpoint := "telos.firehose.pinax.network:443"
+	dfuseJWT := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2FwaS5hY2NvdW50LnBpbmF4Lm5ldHdvcmsvdjEvIiwic3ViIjoiZDllMzYwNTUtZDI3OS00ZTQ2LTgwYzgtNjhlYjQ1NmFmYTAyIiwiYXVkIjpbImh0dHBzOi8vYWNjb3VudC5waW5heC5uZXR3b3JrLyJdLCJleHAiOjIwMjA3NzY2ODIsImlhdCI6MTcwNTQxNjY4MiwiYXBpX2tleV9pZCI6ImI3M2JlMjVmLWMyZjctNDlkZC1iZjA4LWEyMDRkZTc5ZTg5ZCJ9.Qdkm1n7yJP7GxHMDL66yfIS7Kr588wfKUasQhYfBBhc"
 
-	client, err := dfclient.NewDfClient(dfuseEndpoint, dfuseAPIKey, dfuseAuthURL, chainEndpoint, &slog.Config{Pretty: true, Level: zerolog.TraceLevel})
+	client, err := dfclient.NewDfClient(dfuseEndpoint, dfuseJWT, &slog.Config{Pretty: true, Level: zerolog.TraceLevel})
 
 	if err != nil {
 		panic(fmt.Sprintln("Error creating client: ", err))
@@ -75,11 +72,11 @@ func main() {
 	// 	// Details: pbbstream.BlockDetails_BLOCK_DETAILS_FULL,
 	// }, &blockStreamHandler{})
 	deltaRequest := &dfclient.DeltaStreamRequest{
-		StartBlockNum: 242834360,
+		StartBlockNum: 320030296,
 		// StartCursor:    "-NZ02QtqUc65KeC9HlF3Q6WwLpcyB11tXQPmLRREj4un9CaTi5_0AmUgPE_Ywfuj3BfoQl-s2NebQHd888FV6tS5lrw163Q_T3wsktrt-OLsLfr3OA0TcuhkDuuMY9DRWjvVagL4frAJ6tW2PqePMxZgMMcvJDe1h2pWpdFccaMX63c9yjr4J8eA0aiV9oQUrbMsEOXzx3qmVmYof04POsSLbvHK6mp2Z3E=__172883755__0__0",
-		StopBlockNum:   0,
-		ForkSteps:      []pbbstream.ForkStep{pbbstream.ForkStep_STEP_NEW, pbbstream.ForkStep_STEP_UNDO},
-		ReverseUndoOps: true,
+		StopBlockNum:    0,
+		FinalBlocksOnly: false,
+		ReverseUndoOps:  true,
 	}
 	// deltaRequest.AddTables("eosio", []string{"payments"})
 	deltaRequest.AddTables("dao.hypha", []string{"documents", "edges"})
